886ac8ff18695b3fd38b7c241298db8fc73f9060
andy-kajeke <akajeke@gmail.com>
2020-04-15 11:35:31 +0300

    generate and scan

---
+++ app/src/main/java/com/planetsystems/barcode/ScannedContent.java
@@ -0,0 +1,24 @@
+package com.planetsystems.barcode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ScannedContent extends AppCompatActivity {
+
+    TextView content;
+    String content_extra;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanned_content);
+
+        content = findViewById(R.id.scan_content);
+
+        Bundle bundle = getIntent().getExtras();
+        content_extra = bundle.getString("scan_content");
+
+        content.setText(content_extra);
+    }
+}
